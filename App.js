71a3ac5388d161185f5c82dba77d002aae75ddc3
sirDonger <ssrko98@gmail.com>
2020-01-14 15:38:18 +0200

    Updated cors to listen port 4200

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:8000"
+    origin: "http://localhost:4200"
 }));
 
 const { tableRouter } = require('./router');
