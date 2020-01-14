cfdab0581055adab7ff420852012180664e10cb1
sirDonger <ssrko98@gmail.com>
2020-01-14 15:21:49 +0200

    Edited delete controller and npm start scripts

--- controllers/tableController/deleteRow.controller.js
+++ controllers/tableController/deleteRow.controller.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
+const objectId = require("mongodb").ObjectID;
 
 const {tableRowSchema} = require('../../database/models');
 
 module.exports = (req, res) => {
     if(!req.body) return res.status(400);
 
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const email = req.body.email;
+    const id = new objectId(req.body.id);
 
     const tableRowModel = mongoose.model("tables", tableRowSchema);
 
-    tableRowModel.findOneAndDelete({firstname, lastname, email}, (err, result) => {
+    tableRowModel.findOneAndDelete({_id: id}, (err, result) => {
         if (err) return res.status(400).send({msg: `Table wasn't deleted: ${err}`});
 
         res.status(200).send({msg: "Table was successfully deleted", result});
